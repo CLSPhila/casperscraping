70d7c75d6df2e7e2903366ca8ea276d58c35f7dc
Michael Hollander (badass) <hollander@gmail.com>
2015-12-16 02:40:34 +0000

    Changed URL that we are hitting to CP from MC.  Added some testing code to better understand what is going on under the hood

--- useCasper.js
+++ useCasper.js
@@ -4,16 +4,20 @@ var nameControls = {lastName: "ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphDy
                     firstName: "ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphDynamicContent$participantCriteriaControl$firstNameControl"}
 
 
-var casper = require("casper").create({verbose:true})
-casper.start("https://ujsportal.pacourts.us/DocketSheets/MC.aspx#", function() {
-    this.test.assertExists("select[name='ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphDynamicContent$searchTypeListControl']")
+var casper = require("casper").create({
+	verbose:true,
+	logLevel: 'debug'
+});
+
+casper.start("https://ujsportal.pacourts.us/DocketSheets/CP.aspx#", function() {
+    this.test.assertExists("select[name='ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphDynamicContent$searchTypeListControl'], 'Selector exists'");
     //so I need to change the "selected" attribute of the options list and then evaluate the __doPostBack method of the options list.
     works = this.evaluate(function() {
-        var element = jQuery("[name='ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphDynamicContent$searchTypeListControl']")
-        var selectedElement = jQuery("option:selected", element).removeAttr("selected")
-        var participantElement = jQuery("option:contains('Participant')",element)
-        participantElement.attr("selected","selected")
-        setTimeout('__doPostBack(\'ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphDynamicContent$searchTypeListControl\',\'\')', 0)
+        var element = jQuery("[name='ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphDynamicContent$searchTypeListControl']");
+        var selectedElement = jQuery("option:selected", element).removeAttr("selected");
+        var participantElement = jQuery("option:contains('Participant')",element);
+        participantElement.attr("selected","selected");
+        setTimeout('__doPostBack(\'ctl00$ctl00$ctl00$cphMain$cphDynamicContent$cphDynamicContent$searchTypeListControl\',\'\')', 0);
     });//end of this.evaluate
 });//end of casper.start
 
